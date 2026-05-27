f8191f45bcffb7850881d20a442a7971bc145303
Günther Gruber <grbrg@gmx.net>
2026-05-27 11:03:49 +0200

    Refactored world simulation to not directly use input

---
+++ core/input_manager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+@export var world_simulation: WorldSimulation
+
+
+
+##
+func _unhandled_input(event: InputEvent) -> void:
+
+	# handle mouse events
+	if event is InputEventMouseButton:
+		var mouse_pos = get_viewport().get_mouse_position()
+		if world_simulation:
+			var grid_index = world_simulation.get_grid_index(mouse_pos)
+			if grid_index != Vector3i.MIN:
+				world_simulation.add_effect(grid_index)
--- world/world_simulation.gd
+++ world/world_simulation.gd
@@ -2,25 +2,25 @@ class_name WorldSimulation
 extends Node3D
 
 
-
-@export var grid: GridMap
 @export var camera: Camera3D
-
+@export var grid: GridMap
 
 @onready var disc = preload("res://levels/experimental/dummy_disc.tscn")
 
 
+
 ##
-func _unhandled_input(event: InputEvent) -> void:
+func add_effect(grid_index: Vector3i) -> void:
+	var marker = disc.instantiate()
+	self.add_child(marker)
+	marker.position = grid.map_to_local(grid_index)
 
-	# handle mouse events
-	if event is InputEventMouseButton:
-		var mouse_pos = get_viewport().get_mouse_position()
-		var target = Helper3D.get_object_at(camera, mouse_pos)
-		if target:
-			var grid_index = grid.local_to_map(target.position)
-			Log.d("Click at " + str(target.position) + " -> Index:" + str(grid_index))
 
-			var marker = disc.instantiate()
-			self.add_child(marker)
-			marker.position = grid.map_to_local(grid_index)
+##
+func get_grid_index(pos: Vector2) -> Vector3i:
+	var target = Helper3D.get_object_at(camera, pos)
+	if target:
+		var grid_index = grid.local_to_map(target.position)
+		return grid_index
+
+	return Vector3i.MIN
